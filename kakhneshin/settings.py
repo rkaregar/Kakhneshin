1e57231030698d2f073d727d10e25f456883d7a7
Arya Kowsary <aryakowsary@gmail.com>
2019-05-26 00:15:24 +0430

    minor change

--- kakhneshin/settings.py
+++ kakhneshin/settings.py
@@ -128,9 +128,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 LOGIN_REDIRECT_URL = '/users/edit-profile/'
 LOGOUT_REDIRECT_URL = '/users/login'
+
