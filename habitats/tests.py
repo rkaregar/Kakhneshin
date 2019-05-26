9a20977288a055925c18bda70f71f255cc7c820d
somagh <aryakowsary@yahoo.com>
2019-05-26 08:39:03 +0430

    minor change

--- habitats/tests.py
+++ habitats/tests.py
@@ -32,6 +32,7 @@ class HabitatSeleniumTest(LiveServerTestCase):
         driver_options = webdriver.ChromeOptions()
         driver_options.add_argument('--headless')
         driver_options.add_argument('--no-sandbox')
+        driver_options.add_argument('--disable-gpu')
         driver_options.add_argument('--disable-dev-shm-usage')
         self.client = SeleniumDjangoTestClient(web_driver=webdriver.Chrome(chrome_options=driver_options),
                                                live_server_url=self.live_server_url)
