34303b7e51f65dfa148942fa2b7bcf9ab30bb4c5
MiladInk <mi.aghajohari@gmail.com>
2019-06-05 15:12:26 +0430

    add migrations

--- habitats/forms.py
+++ habitats/forms.py
@@ -5,4 +5,4 @@ from habitats.models import Habitat
 class CreateHabitatForm(forms.ModelForm):  # TODO: change name to HabitatForm
     class Meta:
         model = Habitat
-        fields = ('name', 'address', 'town', 'cost')
+        fields = ('name', 'address', 'town')
--- habitats/models.py
+++ habitats/models.py
@@ -6,9 +6,9 @@ class Habitat(models.Model):
     name = models.CharField(max_length=200, default='habitat template name')
     address = models.CharField(max_length=500, default='habitat template address')
     town = models.CharField(max_length=50, default='habitat template town')
-    owner = models.ForeignKey(Member, on_delete=models.CASCADE)
+    owner = models.ForeignKey(Member, on_delete=models.CASCADE, null=True)
 
     # TODO: add new additional fields
 
     def __str__(self):
-        return ','.join(map(str, [self.name, self.cost, self.address, self.town]))
+        return ','.join(map(str, [self.name, self.owner, self.address, self.town]))
