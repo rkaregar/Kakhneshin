9aab6f3ae038c57e8baa2881af8fab30e9abf26c
MiladInk <mi.aghajohari@gmail.com>
2019-06-05 15:03:32 +0430

    add owner when adding habitat

--- habitats/models.py
+++ habitats/models.py
@@ -1,12 +1,14 @@
 from django.db import models
+from users.models import Member
 
 
 class Habitat(models.Model):
     name = models.CharField(max_length=200, default='habitat template name')
     address = models.CharField(max_length=500, default='habitat template address')
     town = models.CharField(max_length=50, default='habitat template town')
-    cost = models.PositiveIntegerField(default=0)
+    owner = models.ForeignKey(Member, on_delete=models.CASCADE)
+
+    # TODO: add new additional fields
 
     def __str__(self):
         return ','.join(map(str, [self.name, self.cost, self.address, self.town]))
-
